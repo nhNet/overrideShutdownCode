2b3807bfe594c08f41f8f143080236304554d187
Browsers <38656592+nhNet@users.noreply.github.com>
2018-06-06 07:09:20 +0200

    Update overrideCode.js

--- overrideCode.js
+++ overrideCode.js
@@ -2,4 +2,4 @@ function shutdown_NH_Net_Browser(){
 	window.location="https://nhnet.github.io/overrideShutdownCode/";
 }
 // To shutdown the Browser execute the following...
-shutdown_NH_Net_Browser();
+// shutdown_NH_Net_Browser();
