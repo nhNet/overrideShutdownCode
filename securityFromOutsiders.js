f06bf087f3e95bf5655144b0a0119aa146685d0f
Browsers <38656592+nhNet@users.noreply.github.com>
2018-06-14 21:30:38 +0200

    Create securityFromOutsiders.js

---
+++ securityFromOutsiders.js
@@ -0,0 +1,20 @@
+//Security from external logins below:
+
+// First load all Google's scripts so that the signIn actually works!
+makeMeta("google-signin-scope","profile email");
+makeMeta("google-signin-client_id","398993796104-lq9k21a411mnehe5p94brocp3rs72dr5.apps.googleusercontent.com");
+loadScript("https://nhnet.github.io/management/private/Accounts/accounts.js");
+loadScript("https://apis.google.com/js/platform.js");
+createLoginButton();
+function onSignIn(googleUser) {
+	// Refer to Google using this variable. This is the base for the useful variables for code users.
+    var profile = googleUser.getBasicProfile();
+	// Name  Google User Variables for future use!
+    var profileImg = profile.getImageUrl();
+    var profileName = profile.getName();
+    var profileEmail = profile.getEmail();
+	
+    if (allAccounts.includes(profileEmail===false){
+            document.body.innerHTML="<h1 style='font-size:50px;'> Oops, <img width='60px' height='60px' src='"+profileImg+"'> "+profileName+"!</h1><h1>You didn't have an account! Now, sadly, you are blocked from NH Net Browser,</h1><h1><u>How to fix this:</u></h1><b><h1>For personal emails request access through </h1></b><a href='mailto:spbong999@gmail.com'>this email.</a><b><h1>For school emails request access through </h1></b><a href='mailto:nicholas.hua@kcpupils.org'>this email.</a><button onclick='signOut();'>SignOut</button>";
+    }
+}
