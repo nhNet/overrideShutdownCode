31ce37db385280e5bd5a92b256f9e6e21bfe45f5
Browsers <38656592+nhNet@users.noreply.github.com>
2018-06-23 07:51:59 +0200

    Update overrideCode.js

--- overrideCode.js
+++ overrideCode.js
@@ -12,8 +12,6 @@ function shutdown_NH_Net_Browser(){
 
 makeScript("(function(h,o,t,j,a,r){h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};h._hjSettings={hjid:920192,hjsv:6};a=o.getElementsByTagName('head')[0];r=o.createElement('script');r.async=1;r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;a.appendChild(r);})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');");
 
-loadScript("securityFromOutsiders.js");
-
 // This is unimportant.
 // It is what loads external files...
 // How boring...
